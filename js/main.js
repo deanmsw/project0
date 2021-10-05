24eac2034c220e3e3898e7068b43d170f2254560
Dean Scotthorne <dean@mysuperiorwebsite.com>
2021-10-05 15:22:47 +1100

    project0 second commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+console.log('hello world');
+
+
+$(document).ready(function () {
+
+
+//   $( e.delegateTarget ).html( 'X' ).addClass('x');
+// })
+
+
+
+$('#box-0').on('click', function (e) {
+  console.log(e);
+})
+$('#box-1').on('click', function (e) {
+  console.log(e);
+})
+$('#box-2').on('click', function (e) {
+  console.log(e);
+})
+$('#box-3').on('click', function (e) {
+  console.log(e);
+})
+$('#box-4').on('click', function (e) {
+  console.log(e);
+})
+$('#box-5').on('click', function (e) {
+  console.log(e);
+})
+$('#box-6').on('click', function (e) {
+  console.log(e);
+})
+$('#box-7').on('click', function (e) {
+  console.log(e);
+})
+$('#box-8').on('click', function (e) {
+  console.log(e);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
